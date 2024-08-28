482f2c91b3d42fd278e316d7a9b1de0eefc1162d
Syahrul Azhar <syahrulsyahrul906@gmail.com>
2024-08-28 17:18:23 +0700

    Add List Data

---
+++ lib/listdata.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Listdata extends StatefulWidget {
+  const Listdata({super.key});
+
+  @override
+  State<Listdata> createState() => _ListdataState();
+}
+
+class _ListdataState extends State<Listdata> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
